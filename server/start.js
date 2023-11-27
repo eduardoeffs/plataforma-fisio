f5176bb3f1ddaec2159a86a6d82a16464383496c
Eduardo Effting <84943146+eduardoeffs@users.noreply.github.com>
2023-11-27 11:34:15 -0300

    Update start.js

--- server/start.js
+++ server/start.js
@@ -1,5 +1,5 @@
 const app = require('./server');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
